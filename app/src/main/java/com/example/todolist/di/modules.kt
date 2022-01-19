c247778d9fb3bce1cf0ab067410f796ec0c455ea
1_skat_1 <skat.apps@gmail.com>
2022-01-19 12:33:15 +0300

    add CreateTaskViewModel in modules

--- app/src/main/java/com/example/todolist/di/modules.kt
+++ app/src/main/java/com/example/todolist/di/modules.kt
@@ -1,7 +1,9 @@
 package com.example.todolist.di
 
+import com.example.todolist.createTask.CreateTaskViewModel
+import org.koin.android.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val modules = module {
-
+    viewModel { CreateTaskViewModel() }
 }
