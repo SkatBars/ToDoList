de78cb9206aeedcdd59288e637b50e3e100c26d2
1_skat_1 <skat.apps@gmail.com>
2022-01-20 00:59:27 +0300

    fir bugs with date

--- app/src/main/java/com/example/todolist/utils/DataBindingUtils.kt
+++ app/src/main/java/com/example/todolist/utils/DataBindingUtils.kt
@@ -4,12 +4,13 @@ import android.annotation.SuppressLint
 import android.view.View
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
+import androidx.databinding.BindingConversion
 import java.text.SimpleDateFormat
 import java.util.*
 
 @SuppressLint("SimpleDateFormat")
-@BindingAdapter("setDate")
-fun convertDateToString(view: TextView, date: Date) {
-    val formatter = SimpleDateFormat("Время: h:mm, Дата: dd-MM-yyyy ")
-    view.text = formatter.format(date)
+@BindingConversion()
+fun convertDateToString(date: Date): String {
+    val formatter = SimpleDateFormat("Время: HH:mm, Дата: dd-MM-yyyy ")
+    return formatter.format(date)
 }
