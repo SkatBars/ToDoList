38545f9327add75de75bf27e0e9cc89b8bbfd099
1_skat_1 <skat.apps@gmail.com>
2022-01-20 15:28:08 +0300

    add connection data and layout with dataBinding

--- app/src/main/java/com/example/todolist/utils/DataBindingUtils.kt
+++ app/src/main/java/com/example/todolist/utils/DataBindingUtils.kt
@@ -5,6 +5,8 @@ import android.view.View
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import androidx.databinding.BindingConversion
+import com.example.todolist.R
+import com.example.todolist.data.Task
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -13,4 +15,28 @@ import java.util.*
 fun convertDateToString(date: Date): String {
     val formatter = SimpleDateFormat("Время: HH:mm, Дата: dd-MM-yyyy ")
     return formatter.format(date)
+}
+
+@BindingConversion()
+fun convertBooleanToString(isSuccess: Boolean): String {
+    return if (isSuccess) "Задача выполнена" else "Задача ожидает выполнения"
+}
+
+@BindingAdapter("setStatusColor")
+fun setStatusColor(view: View, task: Task) {
+    view.setBackgroundColor(
+
+        if (task.isSuccess) {
+            view.context.getColor(R.color.green_500)
+        } else {
+            val currentDate = Calendar.getInstance().time
+            if (currentDate.time < task.date.time) {
+                view.context.getColor(R.color.red_500)
+            } else {
+                view.context.getColor(R.color.orange_500)
+            }
+        }
+    )
+
+
 }
