c7e1be3fe721b2287d622e3a57976013acbf24df
1_skat_1 <skat.apps@gmail.com>
2022-01-20 15:37:15 +0300

    create RecyclerView for task

---
+++ app/src/main/java/com/example/todolist/listTask/TaskAdapterRecyclerView.kt
@@ -0,0 +1,40 @@
+package com.example.todolist.listTask
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.example.todolist.R
+import com.example.todolist.data.Task
+import com.example.todolist.databinding.TaskItemLayoutBinding
+
+class TaskAdapterRecyclerView(private val tasks: List<Task>) : RecyclerView.Adapter<TaskAdapterRecyclerView.TaskHolder>() {
+
+    class TaskHolder(private val binding: TaskItemLayoutBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(task: Task) {
+            binding.task = task
+        }
+
+        companion object {
+            fun from(parent: ViewGroup): TaskHolder {
+                val inflater = LayoutInflater.from(parent.context)
+                val binding = TaskItemLayoutBinding.inflate(inflater, parent, false)
+                return TaskHolder(binding)
+            }
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskHolder {
+        return TaskHolder.from(parent)
+    }
+
+    override fun onBindViewHolder(holder: TaskHolder, position: Int) {
+        holder.bind(tasks[position])
+    }
+
+    override fun getItemCount(): Int {
+        return tasks.size
+    }
+
+}
